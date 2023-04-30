be72d4c1e88e0c5f5e912f0ede2dc33e77e717e7
Srey Mai Phou <105780579+Mai0403@users.noreply.github.com>
2023-04-30 22:37:56 +0700

    Add files via upload

---
+++ javascript/home.js
@@ -0,0 +1,10 @@
+
+let subMenu = document.getElementById("subMenu");
+function toggleMenu() {
+    subMenu.classList.toggle("open-menu");
+}
+
+let subMenu1 = document.getElementById("subMenu1");
+function toggleMenu1() {
+    subMenu1.classList.toggle("open-menu1");
+}
